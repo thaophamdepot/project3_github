fe98f0324866633b104245bddfe8824cf9f26697
thaophamdepot <phambichthaobacninh@gmail.com>
2023-01-03 20:52:47 +0700

    docs: Add a new comment in bikeshare.py documentation file

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
+#COMMENT FOR BRANCH 'refactoring'
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
